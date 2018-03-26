5c523b0429ceb7b65f8f1308058ff7ca77da3676
camouflages <wang_yubo163@163.com>
2018-03-26 19:11:24 +0800

    register

--- zhihu.py
+++ zhihu.py
@@ -1,8 +1,11 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for,session
 from  config import *
+from models import User
+from exts import db
 
 app=Flask(__name__)
 app.config.from_object(DevelopmentConfig)
+db.init_app(app)
 
 @app.route('/')
 def index():
@@ -13,13 +16,38 @@ def login():
     if request.method=='GET':
         return render_template('login.html')
     else:
-        pass
+        telephone=request.form.get('telephone')
+        password=request.form.get('password')
+        user=User.query.filter(telephone==telephone,password==password).first()
+        if user:
+            session['user_id']=user.id
+            session.permanent=True
+            return redirect(url_for('index'))
+        else:
+            return u'手机号或者密码错误'
 @app.route('/regist/',methods=['GET','POST'])
 def regist():
     if request.method=='GET':
         return render_template('regist.html')
     else:
-        pass
+        telephone=request.form.get("telephone")
+        username = request.form.get("username")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        user=User.query.filter(User.telephone==telephone).first()
+        if user:
+            return u'该手机号码已经被注册'
+        else:
+            if password1 != password2:
+                return u'两次密码不一致请重新输入'
+            else:
+                user=User(telephone=telephone,username=username,password=password1)
+                db.session.add(user)
+                db.session.commit(user)
+
+                return redirect(url_for('login'))
+
 
 if __name__=='__main__':
     app.run()
