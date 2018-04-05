dc74ed02ae52235743ced47a7d2c699f71db4181
camouflages <wang_yubo163@163.com>
2018-04-05 21:52:23 +0800

    register

--- zhihu.py
+++ zhihu.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request,redirect,url_for,session
 import config
-from models import User
+from models import User,Question
 from exts import db
 from functools import wraps
 
@@ -72,7 +72,14 @@ def question():
     if request.method=='GET':
         return render_template('question.html')
     else:
-        pass
+        title=request.form.get('title')
+        content = request.form.get('content')
+        question=Question(title=title,content=content)
+        user_id=session.get('use_id')
+        user=User.query.filter(User.id==user_id).first()
+        question.author=user
+        db.session.add(question)
+        db.session.commit()
 
 
 if __name__=='__main__':
